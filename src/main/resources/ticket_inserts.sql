03698440681e9191c5ac121564b02dbdbcdad073
Maya Camps <127402122+mayacamps@users.noreply.github.com>
2023-12-19 13:01:55 +0100

    Delete src/main/resources/ticket_inserts.sql

--- src/main/resources/ticket_inserts.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO `flowershop`.`tickets` (`id`, `total`) VALUES ('1', '45.99');
-INSERT INTO `flowershop`.`tickets_has_products` (`idticket`, `idproduct`, `quantity`) VALUES ('1', '1', '1');
-INSERT INTO `flowershop`.`tickets` (`id`, `total`) VALUES ('2', '10.99');
-INSERT INTO `flowershop`.`tickets_has_products` (`idticket`, `idproduct`, `quantity`) VALUES ('2', '1', '1');
-INSERT INTO `flowershop`.`tickets_has_products` (`idticket`, `idproduct`, `quantity`) VALUES ('2', '2', '2');
-INSERT INTO `flowershop`.`tickets` (`id`, `total`) VALUES ('3', '14.00');
-INSERT INTO `flowershop`.`tickets_has_products` (`idticket`, `idproduct`, `quantity`) VALUES ('3', '2', '3');
-INSERT INTO `flowershop`.`tickets_has_products` (`idticket`, `idproduct`, `quantity`) VALUES ('3', '3', '1');
