c9c846b869016bd2604e9af8c378be62093dbbfe
SandyMrtss <bebesubi2@hotmail.com>
2024-01-15 11:48:38 +0100

    hotfix

--- src/main/java/n1exercici1/FlowerShop.java
+++ src/main/java/n1exercici1/FlowerShop.java
@@ -58,10 +58,15 @@ public class FlowerShop {
         }
     }
     public void addProduct() throws IllegalArgumentException{
-        int type = Readers.readInt("Introduce the product type\n" +
-                "1. Decoration\n" +
-                "2. Flower\n" +
-                "3. Tree");
+        int type;
+        do{
+            type = Readers.readInt("Introduce the product type\n" +
+                    "1. Decoration\n" +
+                    "2. Flower\n" +
+                    "3. Tree");
+        }
+        while (type < 1 || type > 3);
+
         String name = Readers.readString("Introduce its name");
         int quantity = Readers.readInt("Introduce its quantity");
         float price = Readers.readFloat("Introduce its price");
